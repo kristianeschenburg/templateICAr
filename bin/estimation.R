5a13d75911d4398d505fabec726962fb0e5599f1
Kristian Eschenburg <keschenb@uw.edu>
2021-07-29 15:50:04 -0700

    script to wrap estimate_template

--- bin/estimation.R
+++ bin/estimation.R
@@ -1,19 +1,23 @@
+#!/usr/bin/env Rscript
+
 library('optparse')
 library('gifti')
-library('templateICAr')
+# library('templateICAr')
 
 option_list = list(
-    make_option(c("-b", "--bold-files"), 
+    make_option(c("-b", "--boldFiles"), 
                 type="character", 
                 default=NULL,
                 help="Path to list of BOLD images.", 
                 metavar="character"),
-    make_option(c("-g", "--group-components"),
+
+    make_option(c("-g", "--gICA"),
                 type="character",
                 default=NULL,
                 help="Path to group-ICA components.",
                 metavar="character"),
-    make_option(c("-o", "--out-names"),
+
+    make_option(c("-o", "--outBase"),
                 type="character",
                 default=NULL,
                 help="Output base name for mean and variance template estimates.",
@@ -22,3 +26,10 @@ option_list = list(
 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
+
+print(opt)
+
+print('Estimating templateICA mean and variance.')
+estimate_template.gifti(gifti_fnames=opt$boldFiles, 
+                        GICA_fname=opt$gICA,
+                        out_fname=opt$outBase)
