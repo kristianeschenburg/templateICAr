bdd69a6694ab8fc55960aba648515c2dd5808535
kristianeschenburg <keschenb@uw.edu>
2021-08-12 12:39:08 -0700

    working templateICA script for single subjects

--- bin/dual_regression.R
+++ bin/dual_regression.R
@@ -35,23 +35,30 @@ opt = parse_args(opt_parser);
 
 print(opt)
 
-if(!file.exists(mwall)) stop(paste0('The bold image file ', opt$boldFile, ' does not exist.'))
-if(verbose) cat('Reading the bold image file.\n')
+if(!file.exists(opt$boldFile)) stop(paste0('The bold image file ', opt$boldFile, ' does not exist.'))
+cat('Reading the bold image file.\n')
 bold_img = read_gifti(opt$boldFile)
 bold_mat = do.call(cbind, bold_img$data)
 
-if(!file.exists(mwall)) stop(paste0('The template networks file ', opt$gICA, ' does not exist.'))
-if(verbose) cat('Reading the tempalte ICA networks file.\n')
+if(!file.exists(opt$gICA)) stop(paste0('The template networks file ', opt$gICA, ' does not exist.'))
+cat('Reading the tempalte ICA networks file.\n')
 template_img = read_gifti(opt$gICA)
 template = do.call(cbind, template_img$data)
 
 V<-nrow(template); Q<-ncol(template)
 
-if(!file.exists(mwall)) stop(paste0('The medial wall file ', opt$mwall, ' does not exist.'))
-if(verbose) cat('Reading the medial wall file.\n')
+if(!file.exists(opt$mwall)) stop(paste0('The medial wall file ', opt$mwall, ' does not exist.'))
+cat('Reading the medial wall file.\n')
 mwall <- as.matrix(read.table(opt$mwall))
 
+cat('Computing dual regression\n')
 DR = dual_reg(bold_mat[mwall,], template[mwall,])
-estimates <- matrix(0,V,Q)
-estimates[mwall,] = t(DR$S)
-for (i in 1:Q) { template_img$data[[i]] = estimates[,i] }
+components <- matrix(0,V,Q)
+components[mwall,] = t(DR$S)
+out_fname_components = paste0(opt$outBase,".DR.Components.func.gii")
+
+component_img <- template_img
+
+cat('Saving subject-level components.\n')
+for (i in 1:Q) { component_img$data[[i]] = components[,i] }
+write_gifti(gii=component_img, out_file=out_fname_components)
---
+++ bin/template_ICA.R
@@ -0,0 +1,104 @@
+#!/usr/bin/env Rscript
+
+library('optparse')
+library('gifti')
+library('templateICAr')
+
+option_list = list(
+    make_option(c("-b", "--boldFile"), 
+                type="character", 
+                default=NULL,
+                help="Path to subject-level BOLD image.", 
+                metavar="character"),
+
+    make_option(c("--templateMean"),
+                type="character",
+                default=NULL,
+                help="Path to template mean.",
+                metavar="character"),
+
+    make_option(c("--templateVariance"),
+                type="character",
+                default=NULL,
+                help="Path to template variance.",
+                metavar="character"),
+
+    make_option(c("-w", "--mwall"),
+                type="character",
+                default=NULL,
+                help="Path to medial wall components.",
+                metavar="character"),
+
+    make_option(c("--iters"), 
+                type="integer", 
+                default=500,
+                help="Number of random normals to generate [default %default]",
+                metavar="character"),
+                
+    make_option(c("--scale"), 
+                action="store_true", 
+                default=TRUE,
+                help="Scale BOLD data by spatial standard deviation."),
+
+    make_option(c("-o", "--outBase"),
+                type="character",
+                default=NULL,
+                help="Output name subject-level dual regression output.",
+                metavar="character")
+);
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+output_dir=dirname(opt$outBase)
+if (!file.exists(output_dir)) {dir.create(output_dir)}
+
+# READ THE MEDIAL WALL FILE
+if(!file.exists(opt$mwall)) stop(paste0('The medial wall file ', opt$mwall, ' does not exist.'))
+cat('Reading the medial wall file.\n')
+mwall <- as.matrix(read.table(opt$mwall))
+
+# GET TEMPLATE MEAN AND VARIANCE (xifti objects)
+if(!file.exists(opt$templateMean)) stop(paste0('The mean estimate file ', opt$templateMean, ' does not exist.'))
+cat('Reading mean estimate file.\n')
+template_mean <- read_gifti(opt$templateMean)
+tempMean <- do.call(cbind, template_mean$data)
+V <- nrow(tempMean); Q <- ncol(tempMean)
+tempMean <- tempMean[mwall,]
+
+if(!file.exists(opt$templateVariance)) stop(paste0('The mean estimate file ', opt$templateVariance, ' does not exist.'))
+cat('Reading mean estimate file.\n')
+template_variance <- read_gifti(opt$templateVariance)
+tempVar <- do.call(cbind, template_variance$data)
+tempVar <- tempVar[mwall,]
+
+# READ IN BOLD TIMESERIES DATA
+if(!file.exists(opt$boldFile)) stop(paste0('The BOLD timeseries file ', opt$boldFile,' does not exist.'))
+cat('Reading in BOLD timeseries data.\n')
+
+BOLD_gifti <- read_gifti(opt$boldFile)
+BOLD_mat <- do.call(cbind, BOLD_gifti$data)
+BOLD_mat <- BOLD_mat[mwall,]
+
+cat('Estimating templateICA on subject-level BOLD data with ', Q, 'components.\n')
+result <- templateICA(template_mean = tempMean,
+                        template_var = tempVar,
+                        BOLD = BOLD_mat,
+                        scale = opt$scale,
+                        meshes = NULL,
+                        maxiter=opt$iters)
+
+est_mean <- est_sigma <- template_mean
+mu <- sigma<- matrix(0,V,Q)
+
+cat('Saving estimated subject mean.\n')
+mu[mwall,] = as.matrix(result$subjICmean)
+for (i in 1:Q) { est_mean$data[[i]] = mu[,i] }
+out_fname_mean = paste0(opt$outBase,".templateICA.Mean.func.gii")
+write_gifti(gii=est_mean, out_file=out_fname_mean)
+
+cat('Saving estimated subject variance.\n')
+sigma[mwall,] = as.matrix(result$subjICvar)
+for (i in 1:Q) { est_sigma$data[[i]] = sigma[,i] }
+out_fname_variance = paste0(opt$outBase,".templateICA.Variance.func.gii")
+write_gifti(gii=est_sigma, out_file=out_fname_variance)
